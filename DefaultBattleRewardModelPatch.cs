8e530991e323879d5a6813b45e545f1e515e36ef
Liam <mipen@windowslive.com>
2020-04-03 13:18:24 +1100

    Update DefaultBattleRewardModelPatch.cs

--- DefaultBattleRewardModelPatch.cs
+++ DefaultBattleRewardModelPatch.cs
@@ -1,5 +1,7 @@
 ﻿using HarmonyLib;
 using Helpers;
+using System;
+using System.Windows.Forms;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
 
@@ -10,24 +12,33 @@ namespace BannerlordTweaks
     {
         static bool Prefix(PartyBase party, float renownValueOfBattle, float contributionShare, StatExplainer explanation, ref float __result)
         {
-            ExplainedNumber stat;
-            if (party.LeaderHero != null && party.LeaderHero == Hero.MainHero)
-                stat = new ExplainedNumber((renownValueOfBattle * contributionShare) * 2f, explanation);
-            else
-                stat = new ExplainedNumber(renownValueOfBattle * contributionShare, explanation);
-            if (party.IsMobile)
+            bool patched = false;
+            try
             {
-                if (party.MobileParty.HasPerk(DefaultPerks.TwoHanded.Notorious))
+                ExplainedNumber stat;
+                if (party.LeaderHero != null && party.LeaderHero == Hero.MainHero)
+                    stat = new ExplainedNumber((renownValueOfBattle * contributionShare) * 2f, explanation);
+                else
+                    stat = new ExplainedNumber(renownValueOfBattle * contributionShare, explanation);
+                if (party.IsMobile)
                 {
-                    PerkHelper.AddPerkBonusForParty(DefaultPerks.TwoHanded.Notorious, party.MobileParty, ref stat);
+                    if (party.MobileParty.HasPerk(DefaultPerks.TwoHanded.Notorious))
+                    {
+                        PerkHelper.AddPerkBonusForParty(DefaultPerks.TwoHanded.Notorious, party.MobileParty, ref stat);
+                    }
+                    if (party.MobileParty.HasPerk(DefaultPerks.Charm.ShowYourScars))
+                    {
+                        PerkHelper.AddPerkBonusForParty(DefaultPerks.Charm.ShowYourScars, party.MobileParty, ref stat);
+                    }
                 }
-                if (party.MobileParty.HasPerk(DefaultPerks.Charm.ShowYourScars))
-                {
-                    PerkHelper.AddPerkBonusForParty(DefaultPerks.Charm.ShowYourScars, party.MobileParty, ref stat);
-                }
+                __result = stat.ResultNumber;
+                patched = true;
             }
-            __result = stat.ResultNumber;
-            return false;
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred during DefaultBattleRewardModelPatch. Reverting to original behaviour...\n\nException:\n{ex.Message}\n\n{ex.InnerException?.Message}\n\n{ex.InnerException?.InnerException?.Message}");
+            }
+            return !patched;
         }
     }
 }
