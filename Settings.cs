fc8818af937789fc31e3d67dab0e6262f8072366
Willem Swanepoel <willem.swanepoel@valtech.com>
2020-05-07 18:05:23 +0200

    added option to adjust caravan size

--- Settings.cs
+++ Settings.cs
@@ -432,5 +432,20 @@ namespace BannerlordTweaks
         [SettingPropertyGroup("Pregnancy Tweaks/Twins Probability Tweak")]
         public float TwinsProbability { get; set; } = 0.03f;
         #endregion
+
+        #region Caravan Patches 
+        [XmlElement]
+        [SettingProperty("Enable Caravan Tweak", "Enables the tweaking of Caravans")]
+        [SettingPropertyGroup("Caravan Tweaks", true)]
+        public bool CaravanTweakEnabled { get; set; } = true;
+        [XmlElement]
+        [SettingProperty("Enable Player Caravan Party Size Tweak", "Applies a configured value to your caravan party size")]
+        [SettingPropertyGroup("Caravan Tweaks", false)]
+        public bool PlayerCaravanPartySizeTweakEnabled { get; set; } = true;
+        [XmlElement]
+        [SettingProperty("Player Caravan Party Size", 30, 100, "Native: 30")]
+        [SettingPropertyGroup("Caravan Tweaks")]
+        public int PlayerCaravanPartySize { get; set; } = 30;
+        #endregion
     }
 }
