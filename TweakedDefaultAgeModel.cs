991f02dac09b61c59198c139218de99ab9d5a0fd
Willem Swanepoel <willem.swanepoel@valtech.com>
2020-05-07 19:51:19 +0200

    moved to the Tweaked Model feature

---
+++ TweakedDefaultAgeModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+
+namespace BannerlordTweaks
+{
+    public class TweakedDefaultAgeModel : DefaultAgeModel
+    {
+        public override int BecomeInfantAge => Settings.Instance.DefaultAgeTweakEnabled
+            ? Settings.Instance.BecomeInfantAge
+            : base.BecomeInfantAge;
+
+        public override int BecomeChildAge => Settings.Instance.DefaultAgeTweakEnabled
+            ? Settings.Instance.BecomeChildAge
+            : base.BecomeChildAge;
+
+        public override int BecomeTeenagerAge => Settings.Instance.DefaultAgeTweakEnabled
+            ? Settings.Instance.BecomeTeenagerAge
+            : base.BecomeTeenagerAge;
+        
+        public override int HeroComesOfAge => Settings.Instance.DefaultAgeTweakEnabled
+            ? Settings.Instance.HeroComesOfAge
+            : base.HeroComesOfAge; 
+        
+        public override int BecomeOldAge => Settings.Instance.DefaultAgeTweakEnabled
+            ? Settings.Instance.BecomeOldAge
+            : base.BecomeOldAge;
+        
+        public override int MaxAge => Settings.Instance.DefaultAgeTweakEnabled
+            ? Settings.Instance.MaxAge
+            : base.MaxAge;
+    }
+}
