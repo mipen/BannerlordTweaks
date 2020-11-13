f2904373b50223a50b0257dbd9769ce655716e37
Jiros <67016716+Jirow13@users.noreply.github.com>
2020-11-13 14:51:03 -0500

    Fixed Bug in Clan Barter that was letting clan leaders switch sides.

--- Patches/BarterablePatches.cs
+++ Patches/BarterablePatches.cs
@@ -31,15 +31,19 @@ namespace BannerlordTweaks.Patches
                 Hero leader = factionForEvaluation.Leader;
 
                 if (leader == null) return;
-
+                
                 int relations = Hero.MainHero.GetRelation(leader);
                 double percent = Math.Abs(((double)(relations) / 100) - 1);
 
                 // Make it very expensive to try to lure a Lord w/ negative relations.
                 double num2 = (relations > -1) ? (__result * percent) : (__result * percent) * 100;
 
+                // Don't let Faction Leaders Defect from their Own Factions
+                if (leader.MapFaction.Leader == leader) num2 *= 1000000;
+
                 __result = (int)(Math.Round(num2));
                 //DebugHelpers.DebugMessage("Relations = " + relations + " | Old = " + old + " | adjusted = " + percent + " | num2 =" + num2 + " | Result = " + __result);
+                //DebugHelpers.DebugMessage("Leader: "+leader.Name+" | MapFaction: "+leader.MapFaction.Name+" | MapFaction Leader: "+leader.MapFaction.Leader.Name);
             }
         }
 
