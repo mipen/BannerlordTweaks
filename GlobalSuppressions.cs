c595e9e55fd9f77da8b4ea0bac94dd51de0728fc
Jiros <67016716+Jirow13@users.noreply.github.com>
2020-12-01 15:46:15 -0500

    Minor cleanup

--- GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>", Scope = "member", Target = "~M:BannerlordTweaks.SubModule.OnGameStart(TaleWorlds.Core.Game,TaleWorlds.Core.IGameStarter)")]
-[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>", Scope = "member", Target = "~M:BannerlordTweaks.DismembermentMissionBehaviour.OnRegisterBlow(TaleWorlds.MountAndBlade.Agent,TaleWorlds.MountAndBlade.Agent,TaleWorlds.Engine.GameEntity,TaleWorlds.MountAndBlade.Blow,TaleWorlds.MountAndBlade.AttackCollisionData@)")]
